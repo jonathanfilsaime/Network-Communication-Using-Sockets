af909fff6e0070399315c8eebeb74c464f552aab
jonathan <jonathanfilsaime@gmail.com>
2016-04-19 15:09:34 -0500

    updates
    bug fixes and updates

--- SocketClient.java
+++ SocketClient.java
@@ -8,6 +8,8 @@
 import java.util.Scanner;
 import java.net.*;
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  *
  * @author jonathan
@@ -17,72 +19,90 @@ public class SocketClient {
     private String Name;
     private int i;
     public int x;
-    String menu =   "1. Display the names of all known users.\n" + "2. Display the names of all currently connected users.\n" + "3. Send a text message to a particular user.\n" +"4. Send a text message to all currently connected users.\n" +"5. Send a text message to all known users.\n" +"6. Get my messages.\n" +"7. Exit.";
+    String menu =   "1. Display the names of all known users.\n" + "2. Display the names of all currently connected users.\n" + "3. Send a text message to a particular user.\n" +"4. Send a text message to all currently connected users.\n" +"5. Send a text message to all known users.\n" +"6. Get my messages.\n" +"7. Exit. \n";
     
     Socket socket = null;
     PrintWriter out = null;
     BufferedReader in = null;
     String temp;
+    String line;
+    int count = 0;
+    int num = 1;
+    String pattern;
+    Pattern r;
+    Matcher m;
     
-   public void communicate()
-   {
-      name();
+    
+    public void communicate()
+    {
+	name();
 
-      //Send data over socket
-      out.println(getName());
+	//Send data over socket
+	out.println(getName());
       
-      do{
-        
-          
-          
-          
+	do{
             //Receive text from server
             try
                 {
-                String line = in.readLine();
-                System.out.println("Text received: " + line);
+                    if(count == 0)
+                    {
+                    line = in.readLine();
+                    System.out.println("Hi " + line);
+                    System.out.print(menu);
+                    x = choice();
+                    exe(x);
+//                    temp = Integer.toString(x);
+//                    out.println(temp);
+                    }
+                    
+                    if(count > 0){
+                        
+                        x = choice();
+                        exe(x);
+                        
+                    }
+                    
+                    if((isInteger(line)) && (x > 7 || x < 1))
+                    {
+                        System.out.println("INVALID INPUT");
+                    }
+                    
                 } 
             
             catch (IOException e)
                 {
-                System.out.println("Read failed");
-                System.exit(1);
+                    System.out.println("Read failed");
+                    System.exit(1);
                 }
 
-            if(x > 7)
-                {
-                System.out.println("INVALID INPUT");
-                }
-          
-          x = choice();
-          temp = Integer.toString(x);
-          out.println(temp);
-      
+        
+        
+        count++;
         }while(x !=7);
       
         System.out.println("out");
-   }
+    }
    
-   public void listenSocket(String host, int port)
-   {
-      //Create socket connection
+    public void listenSocket(String host, int port)
+    {
+	//Create socket connection
       try
-      {
-          socket = new Socket(host, port);
-          out = new PrintWriter(socket.getOutputStream(), true);
-          in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      } 
+	  {
+	      socket = new Socket(host, port);
+	      out = new PrintWriter(socket.getOutputStream(), true);
+	      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	  } 
       catch (UnknownHostException e) 
-      {
-          System.out.println("Unknown host");
-          System.exit(1);
-      } 
+	  {
+	      System.out.println("Unknown host");
+	      System.exit(1);
+	  } 
       catch (IOException e) 
-      {
-          System.out.println("No I/O");
-          System.exit(1);
-      }
-   }
+	  {
+	      System.out.println("No I/O");
+	      System.exit(1);
+	  }
+    }
     
     //set client name 
     public void name()
@@ -94,26 +114,26 @@ public class SocketClient {
     
     //get client name 
     public String getName()
-        {
-            return Name;
-        }
+    {
+	return Name;
+    }
     
     //menu of action choice
     public int choice()
-        {
-            System.out.print(menu);
-            System.out.println("Enter your choice: ");
-            Scanner str = new Scanner(System.in);
-            String userChoice = str.nextLine();
+    {
+//	System.out.print(menu);
+	System.out.println("Enter your choice: ");
+	Scanner str = new Scanner(System.in);
+	String userChoice = str.nextLine();
             
-            if(!(isInteger(userChoice)) || Integer.parseInt(userChoice) < 1 || Integer.parseInt(userChoice) > 7)
+	if(!(isInteger(userChoice)) || Integer.parseInt(userChoice) < 1 || Integer.parseInt(userChoice) > 7)
             {
                 return 0;
             }
             
-            this.i = Integer.parseInt(userChoice);
-            return i;
-        }
+	this.i = Integer.parseInt(userChoice);
+	return i;
+    }
     
     public static boolean isInteger(String s) {
         try {
@@ -127,15 +147,126 @@ public class SocketClient {
         return true;
     }
     
+    public void exe(int p){
+        try{
+            switch (p) {
+                    
+                    case 1:
+                    num = 1;
+                    temp = Integer.toString(x);
+                    out.println(temp);
+                    line = in.readLine();
+                    
+                    pattern = "\\S+";
+                    r = Pattern.compile(pattern);
+                    m = r.matcher(line);
+                    
+                    System.out.println("Know users: ");
+                    
+                    while(m.find( )){
+                        System.out.println("\t" + num + ". " + m.group(0) );
+                        num++;
+                    }
+                    
+                    break;
+                    
+                    case 2:
+                    num = 1;
+                    temp = Integer.toString(x);
+                    out.println(temp);
+                    line = in.readLine();
+                    
+                    pattern = "\\S+";
+                    r = Pattern.compile(pattern);
+                    m = r.matcher(line);
+                    
+                    System.out.println("Know connected users: ");
+                    
+                    while(m.find( )){
+                        System.out.println("\t" + num + ". " + m.group(0) );
+                        num++;
+                    }
+                    
+                    break;
+                    
+                    case 3:
+                    num = 1;
+                    temp = Integer.toString(x);
+                    out.println(temp);
+                    
+                    line = in.readLine();
+                    System.out.print(line);
+                    
+                    Scanner nstr = new Scanner(System.in);
+                    String strname = nstr.nextLine();
+                    out.println(strname);
+                    
+                    line = in.readLine();
+                    System.out.print(line);
+                    String strmessage = nstr.nextLine();
+                    out.println(strmessage);
+                    
+                    line = in.readLine();
+                    System.out.println(line);
+                    
+                    break;
+                    
+                    case 4:
+                    num = 1;
+                    temp = Integer.toString(x);
+                    out.println(temp);
+                    
+                    line = in.readLine();
+                    System.out.print(line);
+                    
+                    Scanner mstr = new Scanner(System.in);
+                    String strmess = mstr.nextLine();
+                    out.println(strmess);
+                    
+                    line = in.readLine();
+                    System.out.println(line);
+                    
+                    break;
+                    
+                    case 5:
+                    num = 1;
+                    temp = Integer.toString(x);
+                    out.println(temp);
+                    
+                    line = in.readLine();
+                    System.out.print(line);
+                    
+                    Scanner mstr1 = new Scanner(System.in);
+                    String strmess1 = mstr1.nextLine();
+                    out.println(strmess1);
+                    
+                    line = in.readLine();
+                    System.out.println(line);
+                    break;
+                    
+                    case 6:
+                    break;
+                    
+                    case 7:
+                    System.exit(0);
+                    break;
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("crash");
+            
+        }
+    }
     
     public static void main(String[] args) 
     {
         
         if (args.length != 2)
-        {
-            System.out.println("Usage:  client hostname port");
-            System.exit(1);
-        }
+	    {
+		System.out.println("Usage:  client hostname port");
+		System.exit(1);
+	    }
 
         SocketClient client = new SocketClient();
 
--- SocketThrdServer.java
+++ SocketThrdServer.java
@@ -10,81 +10,107 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
-
+import java.util.TreeMap;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 class ClientWorker implements Runnable 
 {
-   private Socket client;
+    private Socket client;
+    public int myID;
+    public BufferedReader in = null;
+    public PrintWriter out = null;
+    public String line;
+    public String sentBack = "";
+    public String receiver;
+    public String message;
+    public ArrayList<String> ms = new ArrayList<>();
    
-   ClientWorker(Socket client) 
-   {
-      this.client = client;
-   }
+    ClientWorker(Socket client) 
+    {
+	this.client = client;
+    }
 
-   public void run()
-   {
+    public void run()
+    {
        
        
-      String line;
-      BufferedReader in = null;
-      PrintWriter out = null;
+	
+    
+    
       
        try
-      {
-          in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-          out = new PrintWriter(client.getOutputStream(), true);
-      } 
-      catch (IOException e) 
-      {
-          System.out.println("in or out failed");
-          System.exit(-1);
-      }
+	   {
+	       in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+	       out = new PrintWriter(client.getOutputStream(), true);
+	   } 
+       catch (IOException e) 
+	   {
+	       System.out.println("in or out failed");
+	       System.exit(-1);
+	   }
 
            try
-           {
-               while(true){
-               // Receive text from client
-               line = in.readLine();
-	 
-               // Send response back to client
-                   if(!(isInteger(line)))
-                   {
-                       line = "Hi " + line;
-                       out.println(line);
+	       {
+		   while(true){
+		       // Receive text from client
+		       line = in.readLine();
+		        
+		       // Send response back to client
+		       if(!(isInteger(line)))
+			   {
+                   myID = SocketThrdServer.id;
+                   SocketThrdServer.name[myID] = line;
+                   SocketThrdServer.online[myID] = 1;
+                   
+                   System.out.println(time() +" " + "Connection by known user " + line + ".");
+                   
+                   
+			       line = "Hi " + line;
+                   ms.add(line);
+                   
+                   SocketThrdServer.table.put(SocketThrdServer.name[SocketThrdServer.id], ms);
+                   
+			       out.println(line);
+                   SocketThrdServer.id++;
+			   }
+		       else if(Integer.parseInt(line) < 1 || Integer.parseInt(line) > 7)
+			   {
+			       out.println("please enter a number from 1 to 7");
+			   }
+		       else
+			   {
+			       //past a int here and call a case function
+//			       //************the meat will go here*******
+//			       //out.println(SocketThrdServer.threadArray.size());
+//                   for(int h = 0 ; h < SocketThrdServer.id ; h++){
+//                       System.out.println(SocketThrdServer.table.get(SocketThrdServer.name[h]));
+                   exe(Integer.parseInt(line));
                    }
-                   else if(Integer.parseInt(line) < 1 || Integer.parseInt(line) > 7)
-                   {
-                       out.println("please enter a number from 1 to 7");
-                   }
-                   else
-                   {
-                       //past a int here and call a case function
-                       //************the meat will go here*******
-                       out.println(SocketThrdServer.threadArray.size());
-                   }
+			   }
                 
-               }
+		   
  
-           }
+	       }
            catch (IOException e)
-            {
-                System.out.println("Read failed");
-                System.exit(-1);
-            }
+	       {
+		   System.out.println("Read failed");
+		   System.exit(-1);
+	       }
        
 
       try
-      {
-          client.close();
-      } 
+	  {
+	      client.close();
+	  } 
       catch (IOException e) 
-      {
-          System.out.println("Close failed");
-          System.exit(-1);
-      }
+	  {
+	      System.out.println("Close failed");
+	      System.exit(-1);
+	  }
     
-   }
+    }
     
     public static boolean isInteger(String s) {
         try {
@@ -98,76 +124,197 @@ class ClientWorker implements Runnable
         return true;
     }
 
+    public void exe(int x){
+        try{
+        switch (x) {
+
+                case 1:
+                System.out.println(time() +" "+ SocketThrdServer.name[myID] + " displays all known users.");
+
+                for(int i = 0; i < SocketThrdServer.id; i++){
+                    sentBack += (SocketThrdServer.name[i] + " ");
+                    
+                    }
+                    out.println(sentBack);
+                    sentBack = "";
+                break;
+
+                case 2:
+                System.out.println(time() +" "+ SocketThrdServer.name[myID] + " displays all connected users.");
+
+                for(int i = 0; i < SocketThrdServer.id; i++ ){
+                    if(SocketThrdServer.online[i] == 1){
+                    sentBack += (SocketThrdServer.name[i] + " ");
+                        }
+                    }
+                out.println(sentBack);
+                sentBack = "";
+                break;
+
+                case 3:
+                
+                out.println("name of the receiver ");
+                line = in.readLine();
+                receiver = line;
+
+                out.println("message: ");
+                line = in.readLine();
+                message = line;
+                
+                if(SocketThrdServer.table.containsKey(receiver)){
+                    SocketThrdServer.table.get(receiver).add(message);
+                System.out.println(time() +" "+ SocketThrdServer.name[myID] + " posts a message for " + receiver + ".");
+                }
+                else{
+                    ArrayList<String> ms1 = new ArrayList<>();
+                    SocketThrdServer.id++;
+                    SocketThrdServer.name[SocketThrdServer.id] = receiver;
+                    ms1.add(message);
+                    SocketThrdServer.table.put(SocketThrdServer.name[SocketThrdServer.id], ms1);
+                    System.out.println(time() +" "+ SocketThrdServer.name[myID] + " posts a message for unknow user " + receiver + ".");
+                }
+                
+                out.println("Message posted to " + receiver);
+                    
+                break;
+
+                case 4:
+                out.println("message all currently connected users: ");
+                System.out.println(time() +" "+ SocketThrdServer.name[myID] + " posts a message for all currently connected users.");
+                line = in.readLine();
+                message = line;
+
+                for(int i = 0 ; i < SocketThrdServer.id ; i++){
+                    if(SocketThrdServer.online[i] == 1){
+                        SocketThrdServer.table.get(SocketThrdServer.name[i]).add(message);
+                    }
+                }
+                
+                out.println("Message posted to all currently connected users." );
+                break;
+
+            case 5:
+                out.println("email all users: ");
+                System.out.println(time() +" "+ SocketThrdServer.name[myID] + " posts a message for all known users.");
+
+                line = in.readLine();
+                message = line;
+
+                for(int i = 0 ; i < SocketThrdServer.id ; i++){
+                    SocketThrdServer.table.get(SocketThrdServer.name[i]).add(message);
+                    }
+                
+                out.println("Message posted to all users. " );
+                break;
+
+                case 6:
+                out.println("my messages: ");
+                System.out.println(time() +" "+ SocketThrdServer.name[myID] + "gets messages.");
+
+                for(int i = 0 ; i < SocketThrdServer.table.get(SocketThrdServer.name[myID]).size(); i++){
+                    System.out.println(SocketThrdServer.table.get(SocketThrdServer.name[myID]).get(i));
+                }
+                SocketThrdServer.table.get(SocketThrdServer.name[myID]).removeAll(ms);
+                break;
+
+                case 7:
+                SocketThrdServer.online[myID] = 0;
+                System.out.println(time() +" "+ SocketThrdServer.name[myID] + "exits.");
+
+//                for(int i = 0 ; i < name.size(); i++){
+//                    System.out.println(t.get(name.get(i)));
+//                }
+                System.exit(0);
+                break;
+            }
+        }
+        catch (IOException e)
+        {
+                System.out.println("crash");
+            
+        }
+    
+}
+
+        public String time(){
+                SimpleDateFormat localDateFormat = new SimpleDateFormat("MM/dd/yy h:mm:ss a");
+                String time = localDateFormat.format(new Date());
+                return time;
+                }
+
 }
 
 class SocketThrdServer
 {
-   ServerSocket server = null;
+    ServerSocket server = null;
 
-   public void listenSocket(int port) throws IOException
-   {
+    public void listenSocket(int port) throws IOException
+    {
       try
-      {
-          server = new ServerSocket(port);
-          System.out.println("Server running on port " + port +
-	                     "," + " use ctrl-C to end");
-      } 
-      catch (IOException e) 
-      {
-          System.out.println("Error creating socket");
-          System.exit(-1);
-      }
+	  {
+	      server = new ServerSocket(port);
+	      System.out.println("Server running on port " + port +
+				 "," + " use ctrl-C to end");
+	  }
+      catch (IOException e)
+	  {
+	      System.out.println("Error creating socket");
+	      System.exit(-1);
+	  }
        
       while(true)
-      {
-         ClientWorker w;
+	  {
+	      ClientWorker w;
           
          try
-         {
-                w = new ClientWorker(server.accept());
-                Thread t = new Thread(w);
-                threadArray.add(t);
-                threadArray.get(count++).start();
+	     {
+		 w = new ClientWorker(server.accept());
+		 Thread t = new Thread(w);
+		 threadArray.add(t);
+		 threadArray.get(count++).start();
 
-            }
-        catch (IOException e)
+	     }
+	 catch (IOException e)
           
-            {
-                System.out.println("Accept failed");
-                System.exit(-1);
-            }
-        }
+	     {
+		 System.out.println("Accept failed");
+		 System.exit(-1);
+	     }
+	  }
     }
 
-   protected void finalize()
-   {
+    protected void finalize()
+    {
       try
-      {
-         server.close();
-      } 
+	  {
+	      server.close();
+	  } 
       catch (IOException e) 
-      {
-         System.out.println("Could not close socket");
-         System.exit(-1);
-      }
-   }
+	  {
+	      System.out.println("Could not close socket");
+	      System.exit(-1);
+	  }
+    }
     
+    public static TreeMap<String, ArrayList> table = new TreeMap<>();
     public static ArrayList<Thread> threadArray = new ArrayList<>();
     public static int count = 0;
+    public static int id = 0;
+    public static String[] name = new String[100];
+    public static int[] online = new int[100];
 
-   public static void main(String[] args) throws IOException
-   {
+    public static void main(String[] args) throws IOException
+    {
        
        
-      if (args.length != 1)
-      {
-          System.out.println("Usage: java SocketThrdServer port");
-          System.exit(1);
-      }
+	if (args.length != 1)
+	    {
+		System.out.println("Usage: java SocketThrdServer port");
+		System.exit(1);
+	    }
 
-      SocketThrdServer server = new SocketThrdServer();
-      int port = Integer.valueOf(args[0]);
-      server.listenSocket(port);
-   }
+	SocketThrdServer server = new SocketThrdServer();
+	int port = Integer.valueOf(args[0]);
+	server.listenSocket(port);
+    }
 }
-
